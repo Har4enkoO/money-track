acccb4acc716db674a29b7785595e23a14572643
AntonChetveryk <chetverykanton92@gmail.com>
2020-04-08 19:37:10 +0300

    add categories

--- src/components/Pages/Categories/Categories.js
+++ src/components/Pages/Categories/Categories.js
@@ -5,6 +5,10 @@ class Categories extends React.Component {
   state = {
     categories: [
       { categoryName: "Food", description: "my food", date: "10.04.20" },
+      { categoryName: "Clothes", description: "", date: "10.04.20" },
+      { categoryName: "Restaurants", description: "", date: "10.04.20" },
+      { categoryName: "Utility bills", description: "", date: "10.04.20" },
+      { categoryName: "Pets", description: "", date: "10.04.20" },
     ],
   };
 
@@ -16,14 +20,14 @@ class Categories extends React.Component {
           <Link to="/add_category">Add category</Link>
         </div>
         <div className="table">
-          <div className="row">
+          <div className="row mb-4">
             <div className="col-3 text-center">Category</div>
             <div className="col-3 text-center">Description</div>
             <div className="col-3 text-center">Date</div>
             <div className="col-3 text-center">Action</div>
           </div>
           {this.state.categories.map((category) => (
-            <div className="row" key={category.categoryName}>
+            <div className="row mt-2" key={category.categoryName}>
               <div className="col-3 text-center">{category.categoryName}</div>
               <div className="col-3 text-center">{category.description}</div>
               <div className="col-3 text-center">{category.date}</div>
